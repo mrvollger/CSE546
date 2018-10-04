57fd07c1c52027d87abd0b6cd3ac3f47add16faf
mvollger <mrvollger@gmail.com>
2018-10-03 21:21:45 -0700

    remvoed new lines

--- hw0/hw0.py
+++ hw0/hw0.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 import numpy as np 
 import matplotlib
 matplotlib.use('agg')
@@ -7,21 +6,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set()
 
-
 def Yk(n, k):
 	yk = np.sum(np.sign(np.random.randn(n,k)) * np.sqrt(1./k), axis=1)
 	return(yk)
-
 
 n=100000
 assert np.sqrt(1/(4*n)) < 0.0025
 Z=np.random.randn(n)
 
-
 fig = plt.figure()
-
 plt.step(sorted(Z), np.arange(1,n+1)/float(n) , label = 'Gaussian')
-
 
 ks = [1,8,64,512]
 yks = []
@@ -36,6 +30,3 @@ plt.xlabel('Observations', fontsize=12)
 plt.xlim(-3,3)
 
 fig.savefig('problem9.png')
-
-
-
