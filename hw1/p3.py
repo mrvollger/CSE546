851e77441c650124eb9a388bf8134bd3094a1603
mvollger <mrvollger@gmail.com>
2018-10-05 23:05:02 -0700

    Finished code for Problemmmm 3

--- hw1/p3.py
+++ hw1/p3.py
@@ -13,6 +13,29 @@ sns.set()
 sns.set_style("ticks")
 np.random.seed(0)
 
+def load_data():
+	savedf = "data.pkl"
+	if(os.path.exists(savedf)):
+		print("Reading: " + savedf)
+		with open(savedf, 'rb') as input:
+			X_train = pickle.load(input)
+			labels_train = pickle.load(input)
+			X_test = pickle.load(input)
+			labels_test = pickle.load(input)
+	else:
+		mndata = MNIST('python-mnist/data/')
+		X_train, labels_train = map(np.array, mndata.load_training())
+		X_test, labels_test = map(np.array, mndata.load_testing())
+		X_train = X_train/255.0
+		X_test = X_test/255.0
+		with open(savedf, 'wb') as output:
+			pickle.dump(X_train, output, pickle.HIGHEST_PROTOCOL)
+			pickle.dump(labels_train, output, pickle.HIGHEST_PROTOCOL)
+			pickle.dump(X_test, output, pickle.HIGHEST_PROTOCOL)
+			pickle.dump(labels_test, output, pickle.HIGHEST_PROTOCOL)
+
+	return(X_train, labels_train, X_test, labels_test)
+
 
 def hotones(labels):
 	cats = 10 # numer of catigories
@@ -47,30 +70,6 @@ def accuracy(p, Y):
 	acc = correct / num
 	return(acc)
 
-
-def load_data():
-	savedf = "data.pkl"
-	if(os.path.exists(savedf)):
-		print("Reading: " + savedf)
-		with open(savedf, 'rb') as input:
-			X_train = pickle.load(input)
-			labels_train = pickle.load(input)
-			X_test = pickle.load(input)
-			labels_test = pickle.load(input)
-	else:
-		mndata = MNIST('python-mnist/data/')
-		X_train, labels_train = map(np.array, mndata.load_training())
-		X_test, labels_test = map(np.array, mndata.load_testing())
-		X_train = X_train/255.0
-		X_test = X_test/255.0
-		with open(savedf, 'wb') as output:
-			pickle.dump(X_train, output, pickle.HIGHEST_PROTOCOL)
-			pickle.dump(labels_train, output, pickle.HIGHEST_PROTOCOL)
-			pickle.dump(X_test, output, pickle.HIGHEST_PROTOCOL)
-			pickle.dump(labels_test, output, pickle.HIGHEST_PROTOCOL)
-
-	return(X_train, labels_train, X_test, labels_test)
-
 def splitTrainTest(X, Y):
 	num = X.shape[0]	
 	test_idx = np.random.random_integers(0, high=num-1, size=int(num*.2))		
@@ -86,6 +85,13 @@ def splitTrainTest(X, Y):
 
 
 def moveToCos(X_test, X_train, p):
+	d = X_test.shape[1]
+	G = np.sqrt(0.1) * np.random.randn(p, d)
+	b = np.random.uniform(low=0, high=2*np.pi, size=p)
+	
+	C_test = np.cos( np.transpose(G.dot(np.transpose(X_test))) + b ) 
+	C_train = np.cos( np.transpose(G.dot(np.transpose(X_train)) )+ b ) 
+	return(C_test, C_train)
 
 
 #
@@ -117,14 +123,67 @@ print("Test accuracy:{:.03f}\nTrain accuracy:{:.03f}".format(a_test, a_train))
 # Part d
 #
 X_test, Y_test, X_train, Y_train = splitTrainTest(X_train, labels_train)
-w_hat = train(X_train, Y_train, L)
-p_test = predict(X_test, w_hat)
-p_train = predict(X_train, w_hat)
 
-a_test = accuracy(p_test, Y_test)
-a_train = accuracy(p_train, Y_train)
-print("Test accuracy:{:.03f}\nTrain accuracy:{:.03f}".format(a_test, a_train))
+# read it in if I have already done it
+if(os.path.exists("crossval.txt")):
+	lines = open("crossval.txt").readlines()
+	ps = []
+	acc_test_p = []
+	acc_train_p = []
+	for line in lines:
+		token = line.strip().split()
+		ps.append(int(token[0]))
+		acc_test_p.append(float(token[1]))
+		acc_train_p.append(float(token[2]))
+else:
+	ps = list(range(100, 6000, 100))
+	acc_test_p = []
+	acc_train_p = []
+	for p in ps:
+		C_test, C_train = moveToCos(X_test, X_train, p)
+		w_c = train(C_train, Y_train, L)
 
+		p_test = predict(C_test, w_c)
+		p_train = predict(C_train, w_c)
+
+		a_test = accuracy(p_test, Y_test)
+		a_train = accuracy(p_train, Y_train)
+		acc_test_p.append(a_test)
+		acc_train_p.append(a_train)
+		print("Test accuracy:{:.03f}\tTrain accuracy:{:.03f}\tp:{}".format(a_test, a_train, p))
+	
+	rtn = ""
+	for a,b,c in zip(ps, acc_test_p, acc_train_p):
+		rtn += "{}\t{}\t{}\n".format(a,b,c)
+	open("crossval.txt", "w+").write(rtn)
+
+
+
+fig, ax = plt.subplots(figsize=(16,9))
+sns.lineplot(ps,acc_test_p, ax = ax, label="test", color="red")
+sns.lineplot(ps, acc_train_p, ax = ax, label = "train", color="blue")
+ax.set_xlabel("p")
+ax.set_ylabel("Accuracy")
+plt.legend()
+sns.despine()
+plt.savefig("P3.pdf")
+
+
+
+
+#
+# Part e   
+#
+maxidx = np.argmax(acc_test_p)
+E_test = acc_test_p[maxidx]
+p_hat = ps[maxidx]
+m = X_test.shape[0]
+
+factor = np.sqrt(np.log(2.0/0.05)/(2*m))
+upper = E_test + factor
+lower = E_test - factor
+
+print("CI is: {:.04f} < E[E_test] < {:.04f}\tE_test:{:.04f}\tp_hat:{}".format(lower, upper, E_test, p_hat))
 
 
 
